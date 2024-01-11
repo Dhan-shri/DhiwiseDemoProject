c3acafdf154cf9c38653d495959e284c63ad995e
Dhanshri <dhanshri20@navgurukul.org>
2024-01-11 22:05:47 +0530

    launching activity created

---
+++ app/src/main/java/com/androiddhiwise/app/appcomponents/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.androiddhiwise.app.appcomponents.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.androiddhiwise.app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
